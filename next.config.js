beb4f1e3c31eb61b1cf1d653b821ef6025f3d922
LilitGH14 <lilitgithub@gmail.com>
2024-07-29 20:53:26 +0400

    remove out

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const nextConfig = {
   output: 'export',
   distDir: "build",
+  reactStrictMode: false,
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true,
+ },
 };
 module.exports = nextConfig;
