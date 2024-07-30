cd49ed8d65d1f9b3c07df473c8ff0673dcbb02db
LilitGH14 <lilitgithub@gmail.com>
2024-07-30 14:45:02 +0400

    fix favicon and user settings

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 const nextConfig = {
   output: "export",
-  // distDir: "out",
+  distDir: "out",
   reactStrictMode: false,
   swcMinify: true,
   typescript: {
     ignoreBuildErrors: true,
   },
-  generateBuildId: async () => {
-    return process.env.GIT_HASH;
-  },
-  experimental: {
-    webVitalsAttribution: ['CLS', 'LCP']
-  }
 };
 module.exports = nextConfig;
